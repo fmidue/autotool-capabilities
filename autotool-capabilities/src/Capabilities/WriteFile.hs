9638d51031d6a1b552508348fd60597dc18f6d3a
patritzenfeld <patrick.ritzenfeld@uni-due.de>
2025-07-17 15:35:05 +0200

    add another capability for un-cached file writing

---
+++ autotool-capabilities/src/Capabilities/WriteFile.hs
@@ -0,0 +1,17 @@
+-- | Defines a monad context for writing a file to disc.
+
+module Capabilities.WriteFile (
+  MonadWriteFile (..),
+  ) where
+
+import Control.Monad.Trans.Class        (MonadTrans (lift))
+import Control.OutputCapable.Blocks.Generic (
+  GenericReportT
+  )
+import Data.ByteString                  (ByteString)
+
+class Monad m => MonadWriteFile m where
+  writeToFile :: FilePath -> ByteString -> m FilePath
+
+instance MonadWriteFile m => MonadWriteFile (GenericReportT l o m)  where
+  writeToFile path = lift . writeToFile path
