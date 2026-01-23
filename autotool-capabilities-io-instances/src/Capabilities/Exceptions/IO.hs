1637d7b308e2f6997439cf36c7de3dfacf665032
patritzenfeld <patrick.ritzenfeld@uni-due.de>
2026-01-23 13:45:44 +0100

    add RandT orphan instances from various repos

---
+++ autotool-capabilities-io-instances/src/Capabilities/Exceptions/IO.hs
@@ -0,0 +1,15 @@
+{-# OPTIONS_GHC -Wno-orphans #-}
+{-# LANGUAGE FlexibleInstances #-}
+-- | Defines exception handling instances for IO based randomization.
+
+module Capabilities.Exceptions.IO () where
+
+import Control.Monad.Catch              (MonadCatch(..), MonadThrow (..))
+import Control.Monad.Trans.Class        (lift)
+import Control.Monad.Trans.Random       (RandT, liftCatch)
+
+instance {-# OVERLAPPABLE #-} MonadThrow (RandT g IO) where
+  throwM = lift . throwM
+
+instance {-# OVERLAPPABLE #-} MonadCatch (RandT g IO) where
+  catch = liftCatch catch
