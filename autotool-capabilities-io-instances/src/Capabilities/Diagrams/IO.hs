73a7688fe3249b2bd20d44c3c3359e35e2529517
Patrick Ritzenfeld <patrick.ritzenfeld@uni-due.de>
2026-01-23 14:22:21 +0100

    concretize and move RandT and ExceptT instances (#8)
    * concretize and move RandT and ExceptT instances
    
    * add newly required MonadGraphviz and MonadDiagrams instances
    
    * add RandT orphan instances from various repos
    
    ---------
    
    Co-authored-by: Janis Voigtländer <jvoigtlaender@users.noreply.github.com>

--- autotool-capabilities-io-instances/src/Capabilities/Diagrams/IO.hs
+++ autotool-capabilities-io-instances/src/Capabilities/Diagrams/IO.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE Arrows #-}
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE OverloadedLists #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
@@ -25,6 +26,9 @@ import qualified Graphics.SVGFonts.Fonts (
 
 import Capabilities.Diagrams            (MonadDiagrams (lin, renderDiagram))
 
+import Control.Monad.Trans.Except       (ExceptT)
+import Control.Monad.Random             (RandT)
+import Control.Monad.Trans.Class        (MonadTrans (lift))
 import Data.ByteString.Internal         (w2c)
 import Data.Data                        (Typeable)
 import Diagrams.Backend.SVG             (SVG(SVG))
@@ -65,6 +69,14 @@ import Data.Maybe                       (maybeToList)
 instance MonadDiagrams IO where
   lin = Graphics.SVGFonts.Fonts.lin
   renderDiagram g = encodeUtf8 . LT.toStrict <$> groupSVG (renderSVG (dims2D 400 400) g)
+
+instance MonadDiagrams (RandT g IO) where
+  lin = lift lin
+  renderDiagram = lift . renderDiagram
+
+instance MonadDiagrams (ExceptT e IO) where
+  lin = lift lin
+  renderDiagram = lift . renderDiagram
 
 data SVGOptions = SVGOptions
   { xmlns
--- autotool-capabilities-io-instances/src/Capabilities/Graphviz/IO.hs
+++ autotool-capabilities-io-instances/src/Capabilities/Graphviz/IO.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE FlexibleInstances #-}
 {-# OPTIONS_GHC -Wno-orphans #-}
 -- | Defines the IO instance for capability Graphviz.
 
@@ -11,6 +12,9 @@ import qualified Diagrams.TwoD.GraphViz           as GV (
 
 import Capabilities.Graphviz            (MonadGraphviz (..))
 
+import Control.Monad.Random             (RandT)
+import Control.Monad.Trans.Except       (ExceptT)
+import Control.Monad.Trans.Class        (MonadTrans (lift))
 import Data.GraphViz                    (quitWithoutGraphviz)
 import Data.String.Interpolate          (iii)
 
@@ -22,3 +26,13 @@ instance MonadGraphviz IO where
       |]
   layoutGraph = GV.layoutGraph
   layoutGraph' = GV.layoutGraph'
+
+instance MonadGraphviz (RandT g IO)  where
+  errorWithoutGraphviz = lift errorWithoutGraphviz
+  layoutGraph command = lift . layoutGraph command
+  layoutGraph' params command = lift . layoutGraph' params command
+
+instance MonadGraphviz (ExceptT e IO) where
+  errorWithoutGraphviz = lift errorWithoutGraphviz
+  layoutGraph command = lift . layoutGraph command
+  layoutGraph' params command = lift . layoutGraph' params command
